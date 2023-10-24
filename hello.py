9571b8596d85079460f516b3d35c28383c993d42
francois GIRARD <francoisgirard51@gmail.com>
2023-10-24 11:58:17 +0200

    hello.py now manages space and absence of names

--- hello.py
+++ hello.py
@@ -4,14 +4,24 @@ import sys
 
 def full_name(first_name, last_name):
     """returns the full name"""
-    name = f"{first_name.capitalize()}{last_name.capitalize()}"
-    
+    first_name = first_name.strip()
+    last_name = last_name.strip()
+
+    if first_name and last_name:
+        name = f"{first_name.capitalize()} {last_name.capitalize()}"
+    elif first_name:
+        name = f"{first_name.capitalize()}"
+    elif last_name:
+        name = f"{last_name.capitalize()}"
+    else:
+        name = ""
+
     return name
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         # => ['hello.py']
-        print(f'Hello{full_name("", "")}!')
+        print(f'Hello{full_name(" ", " ")}!')
     elif len(sys.argv) == 2:
         # => ['hello.py', 'John' ]
         print(f'Hello {full_name(sys.argv[1], "")}!')
